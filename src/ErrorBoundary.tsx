ece432f0264feef00aeb497d9f1b39c084d84637
umarkhan905 <itxumarkhan905@gmail.com>
2025-12-02 11:48:51 +0500

    fixing error issue

--- src/ErrorBoundary.tsx
+++ src/ErrorBoundary.tsx
@@ -15,7 +15,8 @@ export class ErrorBoundary extends React.Component<Props, State> {
     this.state = { hasError: false };
   }
 
-  static getDerivedStateFromError(error) {
+  static getDerivedStateFromError(error: unknown) {
+    console.log("Error:", error);
     // Update state so the next render will show the fallback UI.
     return { hasError: true };
   }
